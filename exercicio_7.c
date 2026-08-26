32bef039a0b259bbadedf37c43ea20c07af53903
Emile Cristine <131501669+EmileCristine@users.noreply.github.com>
2026-08-26 08:03:35 -0300

    Atualizar o exercicio_7.c

--- exercicio_7.c
+++ exercicio_7.c
@@ -1,9 +1,28 @@
-// Nome: Emile Cristine Gomes Nogueira
-// RA: 26892562
-
-#include <stdio.h>
-#include <Math.h>
-
-int main() {
+{
+  int h1, h2, m1, m2;
+  
+  printf("Digite o primeiro horario no seguinte formato: HH:MM\n");
+  scanf("%d:%d", &h1, &m1);
+  
+  if (h1<0 || h1>23 || m1<0 || m1>59) {
+    printf("Horário inválido, tente novamente!\n");
+    scanf("%d:%d", &h1, &m1);
+  } else {
+    printf("Digite o segundo horario no seguinte formato: HH:MM\n");
+    scanf("%d:%d", &h2, &m2);
     
+    if (h2<0 || h2>23 || m2<0 || m2>59) {
+      printf("Horário inválido, tente novamente!\n");
+      scanf("%d:%d", &h2, &m2);    
+    } else {
+      if (m1 > m2) {
+        m1 = 60 - m1;
+      }
+    }
+  }
+  
+  int interH = h2 - h1;
+  int interM = m2 - m1;
+  
+  printf("O intervalo entre os horarios eh: %d:%d\n", interH, interM);
 }
