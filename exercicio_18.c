9978884ce6aea38d71660d5a42a1345e4a2f7c99
Emile Cristine <131501669+EmileCristine@users.noreply.github.com>
2026-09-11 17:43:11 -0300

    Update exercicio_18.c

--- exercicio_18.c
+++ exercicio_18.c
@@ -2,9 +2,42 @@
 // RA: 26892562
 // Exercicio 18
 
+// Escreva um programa que dados três inteiros positivos a, b e c, determine o maior valor que pode
+// ser obtido colocando um operador entre a e b e outro entre + e *, podendo os dois ser iguais, 
+// e é permitido usar parenteses. A ordem dos números não pode ser alterada
+
+// a + b + c 
+// a * b * c
+// (a + b) * c 
+// a * (b + c)
+
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
 int main () {
+    int a, b, c;
+
+    printf("Digite o valor de a, b e c: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a < 0 || b < 0 || c < 0) {
+        printf("Os valores não podem ser negativos!");
+        return EXIT_FAILURE;
+    }
+
+    int op1 = a + b + c;
+    int op2 = a * b * c;
+    int op3 = (a + b) * c;
+    int op4 = a * (b + c);
+
+    int maior = op1;
     
+    if (op2 > maior) maior = op2;
+    if (op3 > maior) maior = op3;
+    if (op4 > maior) maior = op4;
+    
+    printf("O maior valor possível é: %d\n", maior);
+
+    return EXIT_SUCCESS;
+
 }
