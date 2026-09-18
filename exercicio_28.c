0e4cd020edeedc68a847c92d0a0a08a824add979
Emile Cristine <131501669+EmileCristine@users.noreply.github.com>
2026-09-18 11:06:16 -0300

    Update exercicio_28.c

--- exercicio_28.c
+++ exercicio_28.c
@@ -1,20 +1,30 @@
-// EXERCICIO 29
+// EXERCICIO 28
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    int n,  divisores, n_perfeito, i = 0;
+    int n=0,  divisores=0, n_perfeito=0;
     printf("Digite o numero: ");
-    scanf("%d", n);
+    scanf("%d", &n);
+    
+    divisores = n -1;
+    
+    while(divisores < n && divisores > 0){
 
-  while (i < n) {
-    i++;
-
-    if(n % divisores == 0) {
-      printf("%d eh divisivel por %d", divisores, n);
+       if(n%divisores == 0) {
+           n_perfeito += divisores;
+       }
+       
+       divisores--;
     }
-}
+    
+    if (n_perfeito == n){
+        printf("%d eh um numero perfeito.", n);
+    } else {
+        printf("%d nao eh perfeito", n);
+    }
+    
   return EXIT_SUCCESS;
 }
