d52927d617a325f443ea6fb8479498f313d653e1
Emile Cristine <131501669+EmileCristine@users.noreply.github.com>
2026-09-18 10:56:32 -0300

    Update exercicio_28.c

--- exercicio_28.c
+++ exercicio_28.c
@@ -1,1 +1,20 @@
+// EXERCICIO 29
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,  divisores, n_perfeito, i = 0;
+    printf("Digite o numero: ");
+    scanf("%d", n);
+
+  while (i < n) {
+    i++;
+
+    if(n % divisores == 0) {
+      printf("%d eh divisivel por %d", divisores, n);
+    }
+}
+  return EXIT_SUCCESS;
+}
