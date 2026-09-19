66e1df2ced88afe8256cab3814cd22a1ded63b6e
Emile Cristine <131501669+EmileCristine@users.noreply.github.com>
2026-09-18 21:46:49 -0300

    Refactor variable names for better clarity
    Renamed variable 'temp_a' to 'new_a' for clarity and updated its usage.

--- exercicio_30.c
+++ exercicio_30.c
@@ -6,17 +6,17 @@
 int main () {
   int a, b, new_b, isSub = 0;
   int divisor_a = 1;
-  int temp_a;
+  int new_a; // serva para nao comprometer o valor do meu a
 
   printf ("Digite o valor de a e b: ");
   scanf("%d %d", &a, &b);
 
   // primeiros precisa-se descobri o tamanho de a para saber quantos dígitos isolar em b
   // sendo assim, se a = 23, divisor_a vai virar 100
-  temp_a = a;
+ new_a = a;
   while (temp_a > 0) {
     divisor_a *= 10;
-    temp_a /= 10;
+    new_a /= 10;
   }
 
   new_b = b;
